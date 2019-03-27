53ff6d033713a98bf9defb85ff6c75307795f5e2
Richard Wild <richardwild76@gmail.com>
2019-03-27 09:41:36 +0000

    Add not_divisible_by

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,14 +1,20 @@
 pub fn is_leap_year(year: i32) -> bool {
     return year.divisible_by(400) ||
-        (year.divisible_by(4) && !year.divisible_by(100))
+        (year.divisible_by(4) && year.not_divisible_by(100))
 }
 
 trait Divisible {
     fn divisible_by(&self, divisor: i32) -> bool;
+    fn not_divisible_by(&self, divisor: i32) -> bool;
 }
 
 impl Divisible for i32 {
+
     fn divisible_by(&self, divisor: i32) -> bool {
         self % divisor == 0
+    }
+
+    fn not_divisible_by(&self, divisor: i32) -> bool {
+        self % divisor != 0
     }
 }
